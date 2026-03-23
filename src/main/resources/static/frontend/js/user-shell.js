9a9a0f67517903547e7c86d4d3dda967151e5542
Bryce <john.domenden@umtc.com.ph>
2026-03-23 16:18:25 +0800

    Fix fixed sidebar with page scrolling (#42)
    Signed-off-by: Bryce <john.domenden@umtc.com.ph>

--- src/main/resources/static/frontend/js/user-shell.js
+++ src/main/resources/static/frontend/js/user-shell.js
@@ -4,7 +4,6 @@ function userIcon(name) {
     create: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M11 5h2v6h6v2h-6v6h-2v-6H5v-2h6z'/></svg>",
     tickets: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M5 5h14v4a2 2 0 0 0 0 6v4H5v-4a2 2 0 0 0 0-6V5zm4 3v2h6V8H9zm0 6v2h6v-2H9z'/></svg>",
     menu: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M4 7h16v2H4zm0 8h16v2H4zm0-4h16v2H4z'/></svg>",
-    bell: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M12 22a2.5 2.5 0 0 0 2.45-2h-4.9A2.5 2.5 0 0 0 12 22zm7-5V11a7 7 0 1 0-14 0v6l-2 2v1h18v-1l-2-2z'/></svg>",
     more: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M12 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z' transform='translate(0 -3)'/></svg>",
     help: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 17.2a1.2 1.2 0 1 1 1.2-1.2 1.2 1.2 0 0 1-1.2 1.2zm1.57-7.44-.7.49A2.14 2.14 0 0 0 12 14h-1.5v-.38a3.3 3.3 0 0 1 1.4-2.7l.97-.72a1.61 1.61 0 1 0-2.57-1.29H8.8a3.11 3.11 0 1 1 4.77 2.85z'/></svg>",
     password: "<svg viewBox='0 0 24 24' aria-hidden='true'><path d='M17 8h-1V6a4 4 0 0 0-8 0v2H7a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-9a2 2 0 0 0-2-2zm-6 0V6a2 2 0 1 1 4 0v2zm1 8.73V18h-2v-1.27a2 2 0 1 1 2 0z'/></svg>",
@@ -49,10 +48,6 @@ function toggleUserHeaderMenu() {
   document.getElementById('userHeaderMenu')?.classList.toggle('hidden');
 }
 
-function toggleUserNotifMenu() {
-  document.getElementById('userNotifMenu')?.classList.toggle('hidden');
-}
-
 function toggleUserSidebar() {
   document.body.classList.toggle('sidebar-hidden');
 }
@@ -80,11 +75,9 @@ function renderUserUtilityHeader() {
   host.innerHTML = `
     <div class='utility-left'>
       <button class='btn btn-ghost icon-btn header-icon-btn' type='button' onclick='toggleUserSidebar()' aria-label='Toggle sidebar' title='Toggle sidebar'>${userIcon('menu')}</button>
-      <a class='utility-brand' href='/dashboard-user.html' aria-label='Go to workspace overview'>AITSM Workspace</a>
+      <a class='utility-brand' href='/dashboard-user.html' aria-label='Go to user dashboard overview'>AITSM Portal</a>
     </div>
     <div class='utility-right'>
-      <button id='userNotifTrigger' class='btn btn-ghost icon-btn header-icon-btn notif-trigger-btn' type='button' onclick='toggleUserNotifMenu()' aria-label='Notifications' title='Notifications'>${userIcon('bell')}<span id='userNotifCount' class='notif-count'>0</span></button>
-      <div id='userNotifMenu' class='admin-header-menu hidden'><div id='userNotifList' class='small'>Loading...</div></div>
       <button id='userMenuTrigger' class='btn btn-ghost icon-btn header-icon-btn' type='button' onclick='toggleUserHeaderMenu()' aria-label='Open account menu' title='Account menu'>${userIcon('more')}</button>
       <div id='userHeaderMenu' class='admin-header-menu hidden'>
         <button type='button' class='menu-action-btn' onclick='openUserHelp()'>${userIcon('help')}<span>Help</span></button>
