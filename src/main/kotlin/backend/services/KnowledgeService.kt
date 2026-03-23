fe6142d18cef9e89c8c152df163f7cbd33f85c1d
Bryce <john.domenden@umtc.com.ph>
2026-03-23 12:51:39 +0800

    Improve asset status sync and knowledge base management (#32)

--- src/main/kotlin/backend/services/KnowledgeService.kt
+++ src/main/kotlin/backend/services/KnowledgeService.kt
@@ -8,4 +8,7 @@ class KnowledgeService(private val repository: KnowledgeRepository, private val 
     fun list() = repository.list()
     fun create(req: KnowledgeRequest, userId: Int?) = repository.create(req).also { auditRepository.log(userId, "Created article #${it.id}", "knowledge_articles") }
     fun update(id: Int, req: KnowledgeRequest, userId: Int?) = repository.update(id, req).also { auditRepository.log(userId, "Updated article #$id", "knowledge_articles") }
+    fun delete(id: Int, userId: Int?): Boolean = repository.delete(id).also { deleted ->
+        if (deleted) auditRepository.log(userId, "Deleted article #$id", "knowledge_articles")
+    }
 }
